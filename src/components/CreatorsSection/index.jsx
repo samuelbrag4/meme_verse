449acd9da4fdaa0ea647addd3ed9ec50a3818a6d
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2025-04-03 14:57:17 -0300

    feat: create CreatorCard component and integrate it into CreatorsSection

--- src/components/CreatorsSection/index.jsx
+++ src/components/CreatorsSection/index.jsx
@@ -1,3 +1,4 @@
+import CreatorCard from "../CreatorCard";
 import styles from "./CreatorsSection.module.css";
 
 const CreatorsSection = () => {
@@ -7,21 +8,7 @@ const CreatorsSection = () => {
       <div className={styles.creatorsGrid}>
         {topCreators.map((creator) => (
           // COMPONENTE: CreatorCard
-          <div key={creator.id} className={styles.creatorCard}>
-            <img
-              src={creator.avatar}
-              alt={creator.name}
-              className={styles.creatorAvatar}
-            />
-            <h3 className={styles.creatorName}>{creator.name}</h3>
-            <p className={styles.creatorBio}>{creator.bio}</p>
-            <div className={styles.creatorStats}>
-              <span className={styles.creatorFollowers}>
-                {creator.followers} seguidores
-              </span>
-            </div>
-            <button className={styles.followButton}>Seguir</button>
-          </div>
+          <CreatorCard />
           // FIM COMPONENTE: CreatorCard
         ))}
       </div>

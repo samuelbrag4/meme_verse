9d7663c0bc2545db89ae5d160da9a94a60bfb39c
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2025-04-03 14:37:39 -0300

    feat: create EventCard component and integrate it into Sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,3 +1,4 @@
+import EventCard from '../EventCard';
 import styles from './Sidebar.module.css';
 
 const Sidebar = () => {
@@ -7,14 +8,7 @@ const Sidebar = () => {
             <h3 className={styles.sidebarTitle}>Eventos Próximos</h3>
             {upcomingEvents.map((event) => (
               // COMPONENTE: EventCard
-              <div key={event.id} className={styles.eventCard}>
-                <h4 className={styles.eventTitle}>{event.title}</h4>
-                <p className={styles.eventDate}>{event.date}</p>
-                <p className={styles.eventParticipants}>
-                  {event.participants} participantes
-                </p>
-                <button className={styles.eventButton}>Participar</button>
-              </div>
+              <EventCard />
               // FIM COMPONENTE: EventCard
             ))}
           </div>
